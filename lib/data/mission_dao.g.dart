83afa7369739b65f43c23ba6bb0528a21a7467cb
MayoFaro <groodep@yahoo.fr>
2025-08-20 17:54:26 +0100

    finalisation missionslist

---
+++ lib/data/mission_dao.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mission_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$MissionDaoMixin on DatabaseAccessor<AppDatabase> {
+  $MissionsTable get missions => attachedDatabase.missions;
+  $UsersTable get users => attachedDatabase.users;
+}
