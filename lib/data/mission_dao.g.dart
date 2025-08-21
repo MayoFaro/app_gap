38fe8cf8514b448786362edd54b1ae8e47602f95
MayoFaro <groodep@yahoo.fr>
2025-08-21 08:17:02 +0100

    chore: remove local/generated Firebase & functions config files from repo

--- lib/data/mission_dao.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'mission_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$MissionDaoMixin on DatabaseAccessor<AppDatabase> {
-  $MissionsTable get missions => attachedDatabase.missions;
-  $UsersTable get users => attachedDatabase.users;
-}
